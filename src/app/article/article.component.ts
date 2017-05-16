91189c7be7b063774e66550f31fc36e91c31b3dd
Matt Wagner <m.wagner@dwdiesel.com>
2017-05-16 08:31:46 -0400

    Update components to display multiple articles

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,8 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  Input,
+  OnInit } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -8,13 +12,11 @@ import { Article } from './article.model';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  article: Article;
+  @Input() article: Article;
 
   constructor() {
-    this.article = new Article(
-    'Angular 2',
-    'http://angular.io',
-    10);
+    // article is populated by the Input,
+    // so we don't need anything here.
   }
 
   voteUp() {
